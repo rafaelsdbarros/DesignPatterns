da9879cb78332daac6396ee6f0a6baf1a201dcfb
rafaelsdbarros <r4f4el1994@gmail.com>
2024-11-12 18:06:07 -0300

    Adapter pattern update.

--- DesignPatters.playground/Pages/Adapter.xcplaygroundpage/Contents.swift
+++ DesignPatters.playground/Pages/Adapter.xcplaygroundpage/Contents.swift
@@ -1,7 +1,84 @@
-//: [Previous](@previous)
+import UIKit
+import XCTest
 
-import Foundation
+//Common payment card service
+protocol PaymentServiceCard {
+    func debit() -> String
+    func credit() -> String
+}
 
-var greeting = "Hello, playground"
+//Common payment pix service
+protocol PixServicePayment {
+    func pix() -> String
+}
 
-//: [Next](@next)
+//Store 1 that is newer and implements payment service card .
+struct Store1: PaymentServiceCard {
+    
+    let name: String = "GameStore"
+    
+    func debit() -> String {
+        return "[Payment - debit]: Payment successfully!"
+    }
+    
+    func credit() -> String {
+        return "[Payment - credit]: Payment successfully!"
+    }
+    
+}
+
+//Store 2 is more older and need to be updated with new payment services.
+struct Store2 {
+    let name: String = "GameShark"
+    
+    func money() -> String {
+        return "[Payment - money]: Payment successfully!"
+    }
+}
+
+
+//Adapter enters here to implements the interfaces card services and keep the legacy of money payments.
+extension Store2: PaymentServiceCard, PixServicePayment {
+    
+    func debit() -> String {
+        return "[Payment - debit]: Payment successfully!"
+    }
+    
+    func credit() -> String {
+        return "[Payment - credit]: Payment successfully!"
+    }
+    
+    func pix() -> String {
+        return "[Payment - pix]: Payment successfully!"
+    }
+}
+
+struct Client {
+    private let name: String
+    
+    init (name: String) {
+        self.name = name
+    }
+    
+    func getName() -> String {
+        return self.name
+    }
+}
+
+class AdapterTest: XCTestCase {
+    func testAdapter() {
+        let client1 = Client(name: "Rafael")
+        let client2 = Client(name: "Joseph")
+
+        let store1 = Store1()
+        let store2 = Store2()
+
+
+        print("Client \(client1.getName()) buy a notebook in the \(store1.name). \n \(store1.debit())")
+        print("Client \(client2.getName()) buy a ps4 in the \(store2.name). \n \(store2.credit())")
+    }
+}
+AdapterTest.defaultTestSuite.run()
+
+
+
